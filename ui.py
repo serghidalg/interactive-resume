9f50ff76c40c1ab95a54a5f7ff5432883af79c87
root <root@ai-llama.serghidalg>
2023-12-27 23:41:15 +0000

    showing picture of myself in sidebar

--- ui.py
+++ ui.py
@@ -3,6 +3,7 @@ import streamlit as st
 def setup_sidebar():
     st.title("Sergio Hidalgo")
     st.subheader("Curriculum Vitae")
+    st.image("./files/serg_white_cut.png")
     st.markdown("Relevant links:")
     st.markdown("- [LinkedIn](https://www.linkedin.com/in/serghidalg/)")
     st.markdown("- [Github](https://www.github.com/serghidalg)")
