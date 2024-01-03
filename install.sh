350472299eb92d8b0503aaf16d4f6a3f1162dffc
root <root@ai-llama.serghidalg>
2024-01-03 19:27:16 +0000

    making the install agnostic to python name

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
-#!/bin/sh
-python -m venv venv
+#!/bin/bash
+
+
+#!/bin/bash
+
+# Verificar si python3 está instalado
+if command -v python3 &>/dev/null; then
+    python_executable="python3"
+elif command -v python &>/dev/null; then
+    python_executable="python"
+else
+    echo "Python 3 no está instalado."
+    exit 1
+fi
+
+# Crear el entorno virtual
+$python_executable -m venv venv
+
+# Activar el entorno virtual
 source venv/bin/activate
+
+# Instalar los requisitos
 pip install -r requirements.txt
-python setup_variables.py
+
+# Ejecutar setup_variables.py
+$python_executable setup_variables.py
+
